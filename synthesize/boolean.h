1f3d6d9c5ee90e3ba6570930d471d3ed5f27da26
Ned Bingham <edward.bingham@broccolimicro.io>
2023-07-09 21:34:33 -0400

    updating to new boolean

--- synthesize/boolean.h
+++ synthesize/boolean.h
@@ -9,7 +9,7 @@
 
 #include <common/standard.h>
 #include <ucs/variable.h>
-#include <boolean/number.h>
+#include <boolean/unsigned_int.h>
 
 boolean::unsigned_int decompose_hfactor(boolean::unsigned_int c, int w, map<boolean::cube, int> &factors, ucs::variable_set &vars, vector<int> hide);
 boolean::unsigned_int decompose_xfactor(boolean::unsigned_int c, int w, map<boolean::cube, int> &factors, ucs::variable_set &vars, vector<int> hide);
